d1149587670b56d11ddd846b600c3a175d63c877
Lars Willighagen <lars.willighagen@gmail.com>
2020-12-28 14:14:54 +0100

    chore(docs): setup JSDoc

---
+++ .jsdoc.js
@@ -0,0 +1,31 @@
+const package = require('./package.json')
+const version = package.version.match(/(0\.)*\d+/)[0]
+
+module.exports = {
+  plugins: ['plugins/markdown'],
+  markdown: {
+    idInHeadings: true
+  },
+  tags: {
+    allowUnknownTags: true,
+    dictionaries: ['jsdoc', 'closure']
+  },
+  source: {
+    include: [
+      'index.js',
+      'parser.js'
+    ]
+  },
+  opts: {
+    destination: './docs/' + version + '/',
+    encoding: 'utf8',
+    readme: './README.md'
+  },
+  templates: {
+    default: {
+      includeDate: true
+    },
+    cleverLinks: false,
+    monospaceLinks: false
+  }
+}
