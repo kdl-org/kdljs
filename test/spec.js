eabffa3ebfd89b369503f704a3cad94819aab59a
Lars Willighagen <lars.willighagen@gmail.com>
2021-08-31 23:28:43 +0200

    test(kdl4j): add kdl4j test suite

--- test/spec.js
+++ test/spec.js
@@ -4,7 +4,7 @@ const assert = require('assert')
 const path = require('path')
 const fs = require('fs')
 const suite = require('./suite.json')
-const { parse } = require('../')
+const { parse, format } = require('../')
 
 function getInput (name) {
   const fileName = path.join(__dirname, './kdl', name + '.kdl')
@@ -33,6 +33,132 @@ describe('parses', function () {
 
       const expected = prepareExpectations(suite[thing])
       assert.deepStrictEqual(actual.output, expected)
+    })
+  }
+})
+
+const KDL4J_BROKEN_TESTS = new Set([
+  // Nodes cannot have multiline comments between values/props.
+  'asterisk_in_block_comment.kdl',
+  'block_comment.kdl',
+  'block_comment_after_node.kdl',
+  'multiline_comment.kdl',
+  'nested_block_comment.kdl',
+  'nested_comments.kdl',
+  'nested_multiline_block_comment.kdl',
+  'newlines_in_block_comment.kdl',
+
+  // Unclear? If the escline was on the previous line it would continue
+  // node1 but then the schema would require a node separator before node2,
+  // as the newline of the escline itself does not count. If the escline is
+  // on its own line (as it is now) it is not an escline, but syntax unknown
+  // to me.
+  'escline_comment_node.kdl',
+
+  // Single-line comments must have a trailing newline (i.e. cannot
+  // be at the EOF)
+  'only_line_comment.kdl',
+
+  // Only whitespace is supported between /- and the thing it is
+  // commenting out
+  'slashdash_arg_before_newline_esc.kdl'
+])
+
+const KDL4J_BROKEN_OUTPUT_TESTS = new Set([
+  // Different integer formats
+  'binary.kdl',
+  'binary_trailing_underscore.kdl',
+  'binary_underscore.kdl',
+  'leading_zero_oct.kdl',
+  'octal.kdl',
+  'trailing_underscore_hex.kdl',
+  'trailing_underscore_octal.kdl',
+  'underscore_in_octal.kdl',
+
+  // kdljs does not distinguish between empty child blocks and a
+  // missing child block
+  'empty_child.kdl',
+  'empty_child_different_lines.kdl',
+  'empty_child_same_line.kdl',
+  'empty_child_whitespace.kdl',
+  'slashdash_node_in_child.kdl',
+
+  // Different float formats
+  'hex.kdl',
+  'hex_int.kdl',
+  'hex_int_underscores.kdl',
+  'hex_leading_zero.kdl',
+  'leading_zero_binary.kdl',
+  'negative_exponent.kdl',
+  'negative_float.kdl',
+  'no_decimal_exponent.kdl',
+  'numeric_prop.kdl',
+  'positive_exponent.kdl',
+  'slashdash_negative_number.kdl',
+  'underscore_in_exponent.kdl',
+  'underscore_in_float.kdl',
+  'zero_float.kdl',
+
+  // Different float+integer formats
+  'parse_all_arg_types.kdl',
+
+  // Limitations of JavaScript numbers
+  'sci_notation_large.kdl',
+  'sci_notation_small.kdl'
+])
+
+const KDL4J_OPTIONS = {
+  escapes: {},
+  requireSemicolons: false,
+  escapeNonAscii: false,
+  escapeNonPrintableAscii: true,
+  escapeCommon: true,
+  escapeLinespace: true,
+  newline: '\n',
+  indent: 4,
+  indentChar: ' ',
+  exponentChar: 'E',
+  printEmptyChildren: false,
+  printNullArgs: true,
+  printNullProps: true
+}
+
+const KDL4J_PATH = path.join(__dirname, 'kdl4j', 'src', 'test', 'resources', 'test_cases')
+const KDL4J_INPUT_PATH = path.join(KDL4J_PATH, 'input')
+const kdl4jInput = fs.readdirSync(KDL4J_INPUT_PATH)
+
+describe('kdl4j', function () {
+  for (const file of kdl4jInput) {
+    if (KDL4J_BROKEN_TESTS.has(file)) continue
+
+    describe(file, function () {
+      const input = fs.readFileSync(path.join(KDL4J_INPUT_PATH, file), 'utf8')
+      const expectedPath = path.join(KDL4J_PATH, 'expected_kdl', file)
+
+      if (fs.existsSync(expectedPath)) {
+        const expected = fs.readFileSync(expectedPath, 'utf8')
+
+        if (KDL4J_BROKEN_OUTPUT_TESTS.has(file)) {
+
+          it('parses', function () {
+            const { output, errors } = parse(input)
+            const parsedExpected = parse(expected).output
+            assert.deepStrictEqual(errors, [])
+            assert.deepStrictEqual(output, parsedExpected)
+          })
+        } else {
+          it('parses and formats', function () {
+            const { output, errors } = parse(input)
+            assert.deepStrictEqual(errors, [])
+            assert.strictEqual(format(output, KDL4J_OPTIONS), expected)
+          })
+        }
+      } else {
+        it('fails to parse', function () {
+          assert.deepStrictEqual(parse(input).output, undefined)
+          assert.notDeepStrictEqual(parse(input).errors, [])
+        })
+      }
     })
   }
 })
