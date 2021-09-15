e5e38ef3ca0ecde7be007cae6bbe39f623fa0adf
Ben Reinhart <benjreinhart@gmail.com>
2021-09-15 19:18:52 -0400

    fix(parser): do not include non ident chars in idents (#8)

--- parser.js
+++ parser.js
@@ -66,7 +66,7 @@ const Integer = createToken({
 // Other
 const Identifier = createToken({
   name: 'Identifier',
-  pattern: /(?![+-]\d)[\x21-\x27\x2A-\x2E\x3A\x3F-\x5A\x5E-\x7A\x7C\x7E-\uFFFF][\x21-\x27\x2A-\x2E\x30-\x3A\x3F-\x5A\x5E-\x7A\x7C\x7E-\uFFFF]*/
+  pattern: /(?![+-]\d)[\x21\x23-\x27\x2A\x2B\x2D\x2E\x3A\x3F-\x5A\x5E-\x7A\x7C\x7E-\uFFFF][\x21\x23-\x27\x2A\x2B\x2D\x2E\x30-\x3A\x3F-\x5A\x5E-\x7A\x7C\x7E-\uFFFF]*/
 })
 const SemiColon = createToken({ name: 'SemiColon', pattern: /;/ })
 const Equals = createToken({ name: 'Equals', pattern: /=/ })
